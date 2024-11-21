26f5c7eec24ffa6ab0145e8cfed204d365297df4
Chad Lantz <s241783@dtu.dk>
2024-11-21 12:31:09 +0100

    the last commit

--- server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-PATHTOCOMMON=~/os/master/
-# Get the first argument passed to the script, or default to 5002 if no argument is given
-PORT=${1:-5000}
-
-# Run the server with the given or default port
-$(./get-bin-path.sh)/server $PORT
