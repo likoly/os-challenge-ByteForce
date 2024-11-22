cfa9e06ec10173224f605706a320b98fd9ced711
likoly <78964548+likoly@users.noreply.github.com>
2024-11-22 13:58:43 +0100

    Update server.c

--- server.c
+++ server.c
@@ -1,138 +1,138 @@
+#include <netdb.h>
+#include <netinet/in.h>
+#include <openssl/sha.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <openssl/sha.h>
-#include <signal.h>
 
 #include "code/messages.h"
+#include "code/priorityQ.h"
 
 // NOTE: Used https://www.tutorialspoint.com/unix_sockets/client_server_model.htm to understand and build our socket logic
 
-// Global socket variables to be used by handler.
-int server_fd;
-int client_socket;
+// Declare semaphore and mutex
+sem_t requests_in_queue;
+pthread_mutex_t lock;
 
-// CTRL+C termination / interrupt handler
-void terminationHandler(int sig) {
-    close(server_fd);
-    close(client_socket);
-    exit(0);
-}
+// Takes in pointer to priority queue
+void* worker_thread(void *pq) {
 
-// Brute-force search function
-uint64_t bruteForceSearch(uint8_t* hash, uint64_t start, uint64_t end) {
-    uint8_t calculatedHash[32];
-    uint64_t key;
-    for (uint64_t i = start; i < end; i++) {
-        SHA256_CTX sha256;
-        SHA256_Init(&sha256);
-        SHA256_Update(&sha256, &i, 8);
-        SHA256_Final(calculatedHash, &sha256);
-        if (memcmp(hash, calculatedHash, 32) == 0) {
-            key = i;
-            break;
+    // Repeatedly take requests from queue and process them
+    while (1) {
+
+        // Block while queue is empty
+        sem_wait(&requests_in_queue);
+
+        // Extract the request with the highest priority
+        pthread_mutex_lock(&lock);
+        request_t request = extract(pq);
+        pthread_mutex_unlock(&lock);
+
+        // Search for key in given range corresponding to given hash
+        uint8_t calculatedHash[32];
+        uint64_t key;
+        for (key = request.start; key < request.end; key++) {
+            SHA256((uint8_t *)&key, 8, calculatedHash);
+            if (memcmp(request.hash, calculatedHash, 32) == 0)
+                break;
         }
+
+        // Send resulting key back to client and close socket
+        key = be64toh(key);
+        write(request.newsockfd, &key, 8);
+        close(request.newsockfd);
     }
-    return key;
 }
 
+// Start server
 int main(int argc, char *argv[]) {
 
-    // Initialize termination signal
-    signal(SIGINT, terminationHandler);
+    // Initialize Mutex
+    pthread_mutex_init(&lock, NULL);
 
     // Create socket
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Make sure the port is available
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
+    // Set the port as available in case it is not available, and check for error
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
         perror("setsockopt(SO_REUSEADDR) failed");
         exit(1);
     }
 
-    // Initialize socket structure and bind the socket to the port
+    // Initialize socket structure
     struct sockaddr_in serv_addr;
     bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(atoi(argv[1]));
 
-    if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("bind failed");
+    // Bind to host address, and check for error
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR on binding");
         exit(1);
     }
 
     // Listen for client
-    listen(server_fd, 100);
+    listen(sockfd, 100);
 
-    // Declare client address and size
+    // Prepare client address and size
     struct sockaddr_in cli_addr;
-    int cli_lengt = sizeof(cli_addr);
+    int clilen = sizeof(cli_addr);
 
-    // Accept client connections as concurrent child processes
+    // Initialize the priority queue
+    priorityQ_t pq;
+    init_priority_queue(&pq);
+
+    // Initialize thread pool
+    pthread_t threads[4];
+    for (int i = 0; i < 4; i++)
+        pthread_create(&threads[i], NULL, worker_thread, &pq);
+
+    // Initialize request counter
+    int c = 0;
+
+    // Continuously accept requests to populate the priority queue
     while (1) {
 
-        // Accept and check for error
-        client_socket = accept(server_fd, (struct sockaddr *) &cli_addr, &cli_lengt);
-        if (client_socket < 0) {
-            perror("Accept failed");
+        // Accept connection, and check for error
+        int newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0) {
+            perror("ERROR on accept");
             exit(1);
         }
 
-        // Fork off a child process and check for error
-        int pid = fork();
-        if (pid < 0) {
-            perror("Fork failed");
-            exit(1);
-        }
+        // Read in request through new connection
+        uint8_t buffer[PACKET_REQUEST_SIZE];
+        read(newsockfd, buffer, PACKET_REQUEST_SIZE);
 
-        // Child process
-        if (pid == 0) {
+        // Extract components from request
+        uint8_t hash[32];
+        uint64_t start;
+        uint64_t end;
+        uint8_t p;
+        memcpy(hash, buffer + PACKET_REQUEST_HASH_OFFSET, 32);
+        memcpy(&start, buffer + PACKET_REQUEST_START_OFFSET, 8);
+        memcpy(&end, buffer + PACKET_REQUEST_END_OFFSET, 8);
+        memcpy(&p, buffer + PACKET_REQUEST_PRIO_OFFSET, 1);
 
-            // Close the original socket on this process
-            close(server_fd);
+        // Convert start and end byte order
+        start = htobe64(start);
+        end = htobe64(end);
 
-            // Read in request through client socket
-            char buffer[PACKET_REQUEST_SIZE];
-            bzero(buffer, PACKET_REQUEST_SIZE);
-            read(client_socket, buffer, PACKET_REQUEST_SIZE);
+        // Create request object
+        request_t request;
+        init_request(&request, hash, start, end, p, newsockfd, ++c);
 
-            // Declare request components
-            uint8_t hash[32];
-            uint64_t start;
-            uint64_t end;
-            uint8_t p;
+        // Insert request into priority queue
+        pthread_mutex_lock(&lock);
+        insert(&pq, &request);
+        pthread_mutex_unlock(&lock);
 
-            // Extract components from request
-            memcpy(hash, buffer + PACKET_REQUEST_HASH_OFFSET, 32);
-            memcpy(&start, buffer + PACKET_REQUEST_START_OFFSET, 8);
-            memcpy(&end, buffer + PACKET_REQUEST_END_OFFSET, 8);
-            memcpy(&p, buffer + PACKET_REQUEST_PRIO_OFFSET, 1);
-
-            // Convert byte order as needed
-            start = htobe64(start);
-            end = htobe64(end);
-
-            // Perform Brute-force search
-            uint64_t key = bruteForceSearch(hash, start, end);
-
-            // Send back found key to client
-            key = be64toh(key);
-            write(client_socket, &key, 8);
-
-            // Close the child process
-            close(client_socket);
-            exit(0);
-        }
-
-        // Close client socket and begin loop again
-        else {
-            close(client_socket);
-        }
+        // Up the number of requests in queue
+        sem_post(&requests_in_queue);
     }
-
-    return 0;
 }
